61eab0c5998b67c356a4c98b2415110cb884c613
Daniel Paggiola <dpaggiola@gmail.com>
2023-09-27 12:03:26 -0300

    Clase 12

--- my-store/routes/categories.router.js
+++ my-store/routes/categories.router.js
@@ -10,4 +10,52 @@ router.get('/:categoryId/products/:productId', (req, res) => {
   });
 });
 
+router.get('/', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update partially',
+    data: body,
+    id
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id
+  });
+});
+
 module.exports = router;
--- my-store/routes/users.router.js
+++ my-store/routes/users.router.js
@@ -14,4 +14,40 @@ router.get('/', (req, res) => {
   }
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update partially',
+    data: body,
+    id
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id
+  });
+});
+
 module.exports = router;
