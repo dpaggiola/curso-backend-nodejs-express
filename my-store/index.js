bc39e13d4c3377c4acda89298d53ca6c016c89f6
Daniel Paggiola <dpaggiola@gmail.com>
2023-10-20 14:35:03 -0300

    Clase 19

--- my-store/index.js
+++ my-store/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
+
 const app = express();
 const port = 3000;
 
@@ -14,8 +16,12 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
+routerApi(app);
+
+app.use(logErrors); // Debe ir primero porque tiene el next.
+app.use(errorHandler); // Como no tiene next, no se ejecutará nada después.
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });
 
-routerApi(app);
--- my-store/routes/products.router.js
+++ my-store/routes/products.router.js
@@ -14,10 +14,12 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
@@ -41,7 +43,7 @@ router.patch('/:id', async (req, res) => {
     res.json(product);
   } catch (error) {
     res.status(404).json({
-      message: error.message
+      message: error.message,
     });
   }
 });
